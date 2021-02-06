c80ef0005bc521b6e60b2201a1ee79fd599d6227
Erik Andersson <erik.anderzzon@gmail.com>
2021-02-06 10:54:13 +0100

    Initial Commit

---
+++ TheFantasticRace/TheFantasticRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFantasticRaceApp.swift
+//  TheFantasticRace
+//
+//  Created by Erik Westervind on 2021-02-06.
+//
+
+import SwiftUI
+
+@main
+struct TheFantasticRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
